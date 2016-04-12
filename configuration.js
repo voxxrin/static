ef05332b8849d04fa103be50ee8a2d25fbd2c688
Elian ORIOU <walien@users.noreply.github.com>
2016-04-12 22:55:26 +0200

    Create configuration.js

---
+++ configuration.js
@@ -0,0 +1,32 @@
+(function (module) {
+
+    var Envs = {
+        prod: 'http://appv2.voxxr.in',
+        local: ''
+    };
+    Envs._default = Envs.prod;
+
+    /**
+      Edit this part ONLY
+    **/
+    var configuration = {
+        'dev': Envs.local,
+        '0.2': Envs.prod,
+        '0.2.1': Envs.prod
+    };
+    /**************/
+
+    var ServerConfig = {
+        backendUrl: function () {
+            var url = configuration[appVersion];
+            if (url === undefined || url === null) return Envs._default;
+            return url;
+        }
+    };
+
+    module
+        .constant('ServerConfig', ServerConfig)
+        .constant('ServerUrl', ServerConfig.backendUrl())
+        .constant('ServerEnvs', Envs);
+
+})(angular.module('config', []));
